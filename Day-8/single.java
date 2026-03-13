d44af97032f6f6d058951670f0fa520b194072eb
Rekha630 <rekharoyal282@gmail.com>
2026-03-13 20:33:58 +0530

    Implement Singleton pattern in single.java

---
+++ Day-8/single.java
@@ -0,0 +1,16 @@
+class Singleton {
+    private static Singleton instance = new Singleton();
+    private Singleton() {
+    }
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
+
+public class single{
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+    }
+}
