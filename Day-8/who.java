9cd89431ab6c722639e64198ae202f95a0a13810
Rekha630 <rekharoyal282@gmail.com>
2026-03-13 20:35:16 +0530

    who.java to demonstrate power function usage

---
+++ Day-8/who.java
@@ -0,0 +1,7 @@
+import static java.lang.Math.pow;
+class who{
+    public static void main(String[] args){
+        System.out.println(pow(2,2));
+        System.out.println(Math.pow(4,4));    
+    }
+}
